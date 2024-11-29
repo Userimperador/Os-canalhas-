3766a39ddbda4fb143a854d485718bccb061c92d
Userimperador <pedro11000jpgame@gmail.com>
2024-11-28 21:06:09 -0300

    Add files via upload
    Os canalhas

---
+++ script (2).js
@@ -0,0 +1,5 @@
+
+document.getElementById('joinDiscord').addEventListener('click', function () {
+    const discordLink = "https://discord.gg/oscanalhas"; // Substitua pelo link correto
+    window.open(discordLink, "_blank"); // Abre o link em uma nova aba
+});
